9f41cd42a90969eee309ed0fc80b0beabf4acdec
RooN4 <RooN4@tutanota.com>
2019-11-15 14:30:38 +0100

    Added part of script

---
+++ begin.py
@@ -0,0 +1,14 @@
+# These are my changes
+#!/usr/bin/env python
+
+import random
+import time
+import os
+import sys
+
+intervall = 600
+dir = "~/Pictures/"
+
+find = os.popen("find " + dir + " -xtype f")
+
+
